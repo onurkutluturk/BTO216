2d3061d9816fc00fa987307e048fbd9e4463e94b
Gokhan Akcapinar <gokhanakcapinar@gmail.com>
2020-02-22 09:39:47 +0300

    Solution Created for Hafta1

---
+++ Hafta1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hafta1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya!");
+            Console.Read();
+        }
+    }
+}
